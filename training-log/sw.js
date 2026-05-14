d1ff469e94a92a207ce6e76c38cd616f11a9506f
David <david@training-log.app>
2026-05-14 08:44:37 +0000

    Add training log PWA

---
+++ training-log/sw.js
@@ -0,0 +1,42 @@
+const CACHE = 'training-log-v1';
+const ASSETS = [
+  '/training-log/',
+  '/training-log/index.html',
+  '/training-log/manifest.json',
+  '/training-log/icons/icon-192.png',
+  '/training-log/icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS.map(u => {
+      return new Request(u, {cache: 'reload'});
+    })).catch(() => {}))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      const fetchPromise = fetch(e.request).then(response => {
+        if (response && response.status === 200) {
+          const clone = response.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+        }
+        return response;
+      }).catch(() => cached);
+      return cached || fetchPromise;
+    })
+  );
+});
